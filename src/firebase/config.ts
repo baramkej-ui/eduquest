9bbb332afa80925f1f8d8874dfa2d74e84214ebc
김은정 <baramkej@gmail.com>
2025-10-20 06:49:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-480489276-8afd6",
+  "appId": "1:384436203117:web:f03e44747cab2d1d4b9964",
+  "apiKey": "AIzaSyAJAZZLOQ_e-B-svfXdEcrxeVJBOKP2y6k",
+  "authDomain": "studio-480489276-8afd6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "384436203117"
+};
